3d1d7101c04a75f3f5d0cc090651e1550a2da197
Mr0Sajad <sajadbalaei.shamsipour@gmail.com>
2024-11-02 21:35:07 +0330

    add Tamrin_5

---
+++ tamrin_5/tamrin_5/Program.cs
@@ -0,0 +1,28 @@
+﻿
+char key;
+var text = "";
+
+do
+{
+
+    do
+    {
+        key = Console.ReadKey(true).KeyChar;
+
+    } while (char.IsDigit(key) && key != '\r');
+
+    if (key != '\r')
+    {
+        Console.Write(key);
+        text = text + key;
+    }
+
+
+} while (key != '\r');
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(text);
+
+
+
