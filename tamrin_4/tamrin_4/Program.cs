ce3b79eaede8866e9da3e0c0fa1f6f84dd9b345b
Mr0Sajad <sajadbalaei.shamsipour@gmail.com>
2024-11-02 21:34:02 +0330

    add Tamrin_4

---
+++ tamrin_4/tamrin_4/Program.cs
@@ -0,0 +1,33 @@
+﻿
+char key;
+var number = 0;
+
+do
+{
+
+    do
+    {
+        key = Console.ReadKey(true).KeyChar;
+        
+
+    } while (!char.IsDigit(key) && key != '\r');
+
+    if (key != '\r')
+    {
+        Console.Write(key);
+        number = number * 10 + Convert.ToInt32(key.ToString());
+    }
+
+
+} while (key != '\r');
+
+Console.WriteLine();
+Console.WriteLine(number);
+
+
+
+
+
+
+
+
