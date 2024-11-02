20988f49aaa194cfc9ed449792a587b9ae19b52f
Mr0Sajad <sajadbalaei.shamsipour@gmail.com>
2024-11-02 21:32:50 +0330

    add Tamrin_3

---
+++ Tamrin_3/Tamrin_3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Enter n :");
+var n = Convert.ToInt32(Console.ReadLine());
+
+var t = 1;
+var a1 = 0;
+var a2 = 1;
+
+while (true)
+{
+    a1 = a2;
+    a2 = t;
+    t = a1 + a2;
+
+    if (t <= n)
+    {
+        if (t == n)
+        {
+            System.Console.Write("is located");
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    else
+    {
+        System.Console.Write("not located");
+        break;
+
+    }
+}
