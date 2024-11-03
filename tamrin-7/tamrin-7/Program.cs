6d2f496937075e854116b5b25cb99ba56e5daf16
Mr0Sajad <sajadbalaei.shamsipour@gmail.com>
2024-11-03 22:40:28 +0330

    add Tamrin_7

---
+++ tamrin-7/tamrin-7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+var input = Console.ReadLine();
+         
+var matn = @"\b0\d{9}\b";
+
+var matches = Regex.Matches(input, matn);
+var validNumbers = new List<string>();
+
+foreach (Match match in matches)
+{
+    var number = match.Value;
+
+    var distinctDigits = new HashSet<char>(number);
+    if (distinctDigits.Count > 1)
+    {
+        validNumbers.Add(number);
+    }
+}
+        
+        
+Console.WriteLine("Valid phone numbers:");
+foreach (var number in validNumbers)
+{
+    Console.WriteLine(number);
+}
+
